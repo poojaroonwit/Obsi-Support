5c34ac127acba47b185c52892be6ad6d08324f2e
Jaroonwit poolnai <jaroonwit.pool@gmail.com>
2026-08-28 00:46:20 +0700

    style: load SLA workspace styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
 import '../styles/globals.css';
 import '../styles/routing.css';
+import '../styles/sla.css';
 export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
