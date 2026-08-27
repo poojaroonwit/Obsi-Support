5348609841aa266c7e49c9e73124d631a50f9749
Jaroonwit poolnai <jaroonwit.pool@gmail.com>
2026-08-28 00:56:18 +0700

    style: load macro workspace styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
 import '../styles/globals.css';
 import '../styles/routing.css';
+import '../styles/macros.css';
 export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
